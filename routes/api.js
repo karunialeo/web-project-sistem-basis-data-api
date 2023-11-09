a95101cb1bedee2cf6f952ce25503e1ba152eeb4
karunialeo <karunialeo@gmail.com>
2023-11-09 17:14:15 +0700

    add comments in routes

--- routes/api.js
+++ routes/api.js
@@ -16,24 +16,27 @@ import {
 
 const router = express.Router();
 
+// Akses menggunakan postman (install jika belum ada) atau Thunder Client (Extension VS Code)
+// Contoh :
+// http://localhost:3002/api/student
+// jangan lupa memilih method, contoh jika mau mendapatkan list siswa maka menggunakan method GET
+
 // ROUTES FOR STUDENT
 router
   .route("/student")
-  .get(getAllStudents)
-  .post(createStudent)
-  .put(editStudent)
-  .delete(deleteStudent);
-
-router.get("/student/:nis", getStudentByNIS);
+  .get(getAllStudents) // get all student information
+  .post(createStudent) // add new student
+  .put(editStudent) // edit existing student
+  .delete(deleteStudent); // delete student
+router.get("/student/:nis", getStudentByNIS); // get student information by giving NIS as params
 
 // ROUTES FOR TEACHER
 router
   .route("/teacher")
-  .get(getAllTeachers)
-  .post(createTeacher)
-  .put(editTeacher)
-  .delete(deleteTeacher);
-
-router.get("/teacher/:nip", getTeacherByNIP);
+  .get(getAllTeachers) // get all teacher information
+  .post(createTeacher) // add new teacher
+  .put(editTeacher) // edit existing teacher
+  .delete(deleteTeacher); // delete teacher
+router.get("/teacher/:nip", getTeacherByNIP); // get teacher information by giving NIP as params
 
 export default router;
